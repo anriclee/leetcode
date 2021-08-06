e447105ff2a6d5e02fc17b3401c12c62047c0acb
anriclee <anriclee@gmail.com>
2021-08-06 08:19:04 +0800

    same tree

---
+++ java/solution/src/main/java/binarytree/SameTree.java
@@ -0,0 +1,12 @@
+package binarytree;
+
+/**
+ * Developed by Lee Happily.
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p != null && q != null) {
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else return p == null && q == null;
+    }
+}
